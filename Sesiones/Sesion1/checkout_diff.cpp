af51814c3c33e862f51ce17d66d7ba087778cc08
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-15 12:46:00 -0600

    Agregando ejemplo git checkout y git diff
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion1/checkout_diff.cpp
@@ -0,0 +1,13 @@
+/*
+Version del string modificado
+Se utilizo para ejemplificar git checkout y git diff
+*/
+
+// Biblioteca para manejar entrada y salida
+#include <iostream>
+
+int main() {
+    // 
+    std::cout << "Hola a todos y todas, estudiantes de IE0217!";
+    return 0;
+}
