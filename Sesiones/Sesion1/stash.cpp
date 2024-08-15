a6e628af0646db0aef4bb3ede5f2b494d1b17dba
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-15 12:59:41 -0600

    Actualizando el README y stash
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion1/stash.cpp
@@ -0,0 +1,12 @@
+/*
+Modificacion de holaMundo.cpp para probar el comando stash
+*/
+
+// Biblioteca para manejar entrada y salida
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, estudiantes de IE0217!\n";
+    std::cout << "Probando el stash!";
+    return 0;
+}
