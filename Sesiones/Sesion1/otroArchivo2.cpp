9d3ab1af6624a7e5a026ad2b59f21b14c48744ad
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-15 22:11:18 -0600

    Agregando otra linea a otroArchivo.cpp
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion1/otroArchivo2.cpp
@@ -0,0 +1,12 @@
+/*
+Se coloco un string extra para mostrar el funcionamiento
+*/
+
+// Biblioteca para manejar entrada y salida
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, desde otro archivo!\n";
+    std::cout << "Esta es otra linea!";
+    return 0;
+}
