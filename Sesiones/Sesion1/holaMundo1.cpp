a5d78944a5947a95bffcad9e404839cbfa4cf39d
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-15 22:20:49 -0600

    Agregando variaciones de holaMundo.cpp
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion1/holaMundo1.cpp
@@ -0,0 +1,11 @@
+/*
+Version del string en espanol
+*/
+
+// Biblioteca para manejar entrada y salida
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, estudiantes de IE0217!";
+    return 0;
+}
---
+++ Sesiones/Sesion1/holaMundo2.cpp
@@ -0,0 +1,11 @@
+/*
+Version del string modificado para probar git diff
+*/
+
+// Biblioteca para manejar entrada y salida
+#include <iostream>
+
+int main() {
+    std::cout << "Hola a todos y todas, estudiantes de IE0217!";
+    return 0;
+}
