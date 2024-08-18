31ddfeddaa1b629d7d8be59a8cec4778cf3bc37b
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-18 00:39:24 -0600

    Agregando archivo de string
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion2/string.cpp
@@ -0,0 +1,16 @@
+/**
+ * Libreria string para manipular strings en C++
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaracion de variable str tipo string
+    string str = "Hello";
+
+    cout << str; // Imprimir valor de la variable
+    
+    return 0;
+}
