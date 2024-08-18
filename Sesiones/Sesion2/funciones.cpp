00c3b117717f7205dd3d6a5b97f63434c1d9d313
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-18 13:19:54 -0600

    Agregando archivo general funciones
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion2/funciones.cpp
@@ -0,0 +1,22 @@
+/**
+ * Formato funcion
+ * returnType functionName (parameter1, parameter2, ...) {
+ *     Function body
+ * }
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+// Definicion de la funcion
+void greet() {
+    cout << "Hello World";
+}
+
+int main() {
+    // Llamado de la funcion
+    greet();
+
+    return 0;
+}
