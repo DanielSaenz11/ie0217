5f594e0f561a8408026e279c2a284b25170ab942
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-27 17:01:42 -0600

    Agregando header file programa2.hpp
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Tareas/Tarea1/src/programa2.hpp
@@ -0,0 +1,14 @@
+#ifndef PROGRAMA2_HPP
+#define PROGRAMA2_HPP
+
+int calcularSuma(int* arr, int largo);
+
+double calcularPromedio(int* arr, int largo);
+
+int calcularMaximo(int* arr, int largo);
+
+int calcularMinimo(int* arr, int largo);
+
+void buscarNumero(int* arr, int largo);
+
+#endif // PROGRAMA2_HPP
