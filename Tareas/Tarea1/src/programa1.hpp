bc7dec850917a9d8a2e2a78db1d65c8554a36b57
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-29 16:30:15 -0600

    Agregando documentacion y declaracion de funcion mostrarMenu
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea1/src/programa1.hpp
+++ Tareas/Tarea1/src/programa1.hpp
@@ -1,41 +1,157 @@
+/**
+ * @file programa1.hpp
+ * @brief Archivo que contiene las declaraciones para el programa 1.
+ * 
+ * @details Este header file contiene la declaración de los enums y structs utilizados para los tipos
+ * de conversión. Además, se encuentra la firma de las funciones sobrecargadas para la conversión
+ * de unidades de longitud, peso y temperatura.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 30/08/2024
+ */
+
 #ifndef PROGRAMA1_HPP
 #define PROGRAMA1_HPP
 
-// Declaracion de los enums de unidades
-enum Longitud { Metro, Kilometro, Centimetro, Milla, Pulgada };
-enum Peso { Gramo, Kilogramo, Libra, Onza };
-enum Temperatura { Celsius, Fahrenheit, Kelvin };
+#include <iostream>
 
-// Structs con factores de conversion
+/**
+ * @enum opciones_menu
+ * @brief Contiene las opciones del menu del programa.
+ */
+enum opciones_menu {
+    MENU_LONGITUD = 1,
+    MENU_PESO,
+    MENU_TEMPERATURA,
+    MENU_SALIR
+};
+
+/**
+ * @enum longitud
+ * @brief Contiene los tipos de conversión para las unidades de longitud.
+ */
+enum longitud {
+    METRO,
+    KILOMETRO,
+    CENTIMETRO,
+    MILLA,
+    PULGADA
+};
+
+/**
+ * @enum peso
+ * @brief Contiene los tipos de conversión para las unidades de peso.
+ */
+enum peso {
+    GRAMO,
+    KILOGRAMO,
+    LIBRA,
+    ONZA
+};
+
+/**
+ * @enum temperatura
+ * @brief Contiene los tipos de conversión para las unidades de temperatura.
+ */
+enum temperatura {
+    CELSIUS,
+    FAHRENHEIT,
+    KELVIN
+};
+
+/**
+ * @struct ConversionLongitud
+ * @brief Estructura para almacenar los datos de conversión de longitud.
+ * 
+ * Esta estructura contiene las unidades de origen y destino para la conversión entre
+ * unidades de longitud.
+ */
 struct ConversionLongitud {
-    Longitud unidadOrigen;
-    Longitud unidadDestino;
-
-    // Unidad a metro
-    double factorMetro = 1;
-    double factorKilometro = 1000;
-    double factorCentimetro = 0.01;
-    double factorMilla = 1609.34;
-    double factorPulgada = 0.0254;
+    longitud unidadOrigen;
+    longitud unidadDestino;
 };
+
+/**
+ * @struct ConversionPeso
+ * @brief Estructura para almacenar los datos de conversión de peso.
+ * 
+ * Esta estructura contiene las unidades de origen y destino para la conversión entre
+ * unidades de peso.
+ */
 struct ConversionPeso {
-    Peso unidadOrigen;
-    Peso unidadDestino;
-
-    // Unidad a gramo
-    double factorGramo = 1;
-    double factorKilogramo = 1000.0;
-    double factorLibra = 453.592;
-    double factorOnza = 28.3495;
+    peso unidadOrigen;
+    peso unidadDestino;
 };
+
+/**
+ * @struct ConversionTemperatura
+ * @brief Estructura para almacenar los datos de conversión de temperatura.
+ * 
+ * Esta estructura contiene las unidades de origen y destino para la conversión entre
+ * unidades de temperatura.
+ */
 struct ConversionTemperatura {
-    Temperatura unidadOrigen;
-    Temperatura unidadDestino;
+    temperatura unidadOrigen;
+    temperatura unidadDestino;
 };
 
+/**
+ * @brief Convierte unidades de longitud entre sí
+ * 
+ * Esta función permite realizar la conversión de las unidades de conversión: metro,
+ * kilometro, centimetro, milla y pulgada entre sí.
+ * 
+ * @param conversion Struct de tipo ConversionLongitud
+ * @param valor Número al que se quiere realizar la conversión
+ * 
+ * @return Valor convertido resultante
+ */
 double convertir(ConversionLongitud conversion, double valor);
+
+
+/**
+ * @brief Convierte unidades de peso entre sí
+ * 
+ * Esta función permite realizar la conversión de las unidades de conversión: gramo, kilogramo
+ * libra y onza entre sí.
+ * 
+ * @param conversion Struct de tipo ConversionPeso
+ * @param valor Número al que se quiere realizar la conversión
+ * 
+ * @return Valor convertido resultante
+ * @overload
+ */
 double convertir(ConversionPeso conversion, double valor);
+
+
+/**
+ * @brief Convierte unidades de temperatura entre sí
+ * 
+ * Esta función permite realizar la conversión de las unidades de conversión: Celsius, Fahrenheit
+ * y Kelvin entre sí.
+ * 
+ * @param conversion Struct de tipo ConversionTemperatura
+ * @param valor Número al que se quiere realizar la conversión
+ * 
+ * @return Valor convertido resultante
+ * @overload
+ */
 double convertir(ConversionTemperatura conversion, double valor);
 
+/**
+ * @brief Muestra el menú del programa recursivamente.
+ * 
+ * Esta función se encarga de mostrar de forma recursiva el menú del programa.
+ * Se llama dentro de la función main() hasta que se indique la opción de salir.
+ * 
+ * @return void
+ */
+void mostrarMenu();
 
 #endif // PROGRAMA1_HPP
