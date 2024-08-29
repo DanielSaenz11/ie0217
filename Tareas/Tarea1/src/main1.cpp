22c3c30b446ae9a83637d18b6d8689f9d418145b
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-29 16:27:09 -0600

    Agregando licencia y llamada recursiva de menu
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea1/src/main1.cpp
+++ Tareas/Tarea1/src/main1.cpp
@@ -1,59 +1,36 @@
-#include <iostream>
+/**
+ * @file main1.cpp
+ * @brief Archivo que contiene la función main().
+ * 
+ * @details En este archivo se llama la función mostrarMenu(), la cual se repite recursivamente
+ * para poder realizar numerosas conversiones si se desea.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 30/08/2024
+ */
+
 #include "programa1.hpp"
 
 using namespace std;
 
+/**
+ * @brief Punto de entrada principal del programa.
+ * 
+ * Esta función inicializa el programa y gestiona el ciclo principal de ejecución.
+ * 
+ * @return Código de estado del programa.
+ */
 int main() {
-    
-    int opcion;
+    // Repeticion de forma recursiva del menu
+    mostrarMenu();
 
-    int origen, destino;
-    double valor;
-
-    do {
-        cout << "\nConversión de Unidades" << endl;
-        cout << "Opcion 1. Convertir entre diferentes unidades de longitud" << endl;
-        cout << "Opcion 2. Convertir entre diferentes unidades de peso" << endl;
-        cout << "Opcion 3. Convertir entre diferentes unidades de temperatura" << endl;
-        cout << "Opcion 4. Salir" << endl;
-
-        cin >> opcion;
-
-        switch (opcion)
-        {
-        case 1:
-            break;
-        case 2:
-            struct ConversionPeso conversion;
-
-            cout << "Seleccione la unidad de origen (1: Gramo, 2: Kilogramo, 3: Libra, 4: Onza): ";
-            cin >> origen;
-            cout << "Seleccione la unidad de destino (1: Gramo, 2: Kilogramo, 3: Libra, 4: Onza): ";
-            cin >> destino;
-            cout << "Ingrese el valor a convertir: ";
-            cin >> valor;
-
-            conversion.unidadOrigen = (Peso)origen;
-            conversion.unidadDestino = (Peso)destino;
-
-            convertir(conversion, valor);
-            
-            break;
-            
-        case 3:
-            
-            break;
-        case 4:
-            cout << "Saliendo del programa..." << endl;
-            break;
-        
-        default:
-            cout << "Opción inválida. Digite una opción de las mostradas en el menú.";
-        }
-
-    } while (opcion != 4);
-
-
+    cout << "Saliendo del programa..." << endl;
 
     return 0;
 }
