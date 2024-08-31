a992492f19095502ac68f3ad3b77c637ad160454
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-31 17:45:22 -0600

    Agregando tipos de memoria en C++
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion5/tiposMemoria.cpp
@@ -0,0 +1,31 @@
+/**
+ * Tipos de memoria donde se guardan los datos
+ * 
+ * Text: Almacena el codigo donde se esta ejecutando
+ * Data: Almacena variables globales, inicializadas y no inicializadas
+ * Heap: Memoria dinamica para que quien la programe la asigne
+ * Stack: Almacena variables globales
+ */
+
+#include <iostream>
+
+/* Se almacena en el segmento de data*/
+int globalVariable = 42;
+
+int main() {
+    /* Se almacena en el stack */
+    int stackVariable = 10;
+
+    /* Se almacena en el heap */
+    int* heapVariable = new int(20);
+
+    // Imprimir los valores de las variables
+    std::cout << "Valor de globalVariable: " << globalVariable << std::endl;
+    std::cout << "Valor de stackVariable: " << stackVariable << std::endl;
+    std::cout << "Valor de heapVariable: " << heapVariable << std::endl;
+
+    /* Liberar la memoria  asignada en el heap */
+    delete heapVariable;
+
+    return 0;
+}
