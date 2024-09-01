e11ca7c23d3c626e690eb256a529cc19c6191d2b
Daniel Sáenz <dsaenzobando@gmail.com>
2024-08-31 22:54:00 -0600

    Agregando implementacion de lista enlazda
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion5/linkedList.cpp
@@ -0,0 +1,52 @@
+/**
+ * Implementacion de lista enlazada
+ * Un nodo contiene el puntero al siguiente nodo en la lista enlazada
+ */
+
+//#include <bits/stdc++.h> // Incluye bibliotecas estandar de C++, pero no es necesario para la implementacion actual
+#include <iostream>
+using namespace std;
+
+// Declaracion de clase Node
+class Node {
+    public:
+        int value;   // Valor del nodo
+        Node* next;  // Siguiente nodo de la lista
+};
+
+int main() {
+
+    Node* head; // Nodo cabeza de la lista enlazada (para recorrerla)
+    Node* one = NULL;
+    Node* two = NULL;
+    Node* three = NULL;
+
+    // Reservar memoria para los nodos en el heap
+    one = new Node();
+    two = new Node();
+    three = new Node();
+
+    // Asignar valores a nodos
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
+
+    // Conectar nodos: 1 -> 2 -> 3 -> NULL
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    // Imprimir la lista enlazada por valor
+    head = one;
+    while (head != NULL) {
+        cout << head->value;
+        head =  head->next;
+    }
+
+    // Error: No se libera la memoria dinamica
+    delete one;
+    delete two;
+    delete three;
+
+    return 0;
+}
