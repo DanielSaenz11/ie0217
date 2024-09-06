bee3abd32e44f50dc2ccfafcfc1bffde468c2b10
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-06 00:30:46 -0600

    Agregando clase ClientePremium con documentacion
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea2/src/ClientePremium.cpp
+++ Tareas/Tarea2/src/ClientePremium.cpp
@@ -1,9 +1,28 @@
+/**
+ * @file ClientePremium.cpp
+ * @brief Archivo que contiene la implementación de los métodos de la clase ClientePremium.
+ * 
+ * @details En este source file se incluye la definición del constructor de la clase ClientePremium
+ * y una función miembro aplica el descuento al total a pagar para los productos del carrito.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 06/09/2024
+ */
+
+// Header file con la declaración de la clase ClientePremium
 #include "ClientePremium.hpp"
 
 using namespace std;
 
+// Definición del constructor de ClientePremium: Asigna los parámetros recibidos a las variables miembro de la clase
 ClientePremium::ClientePremium(string &nombre, double descuento) : Cliente(nombre), descuento(descuento) {}
 
 double ClientePremium::aplicarDescuento(double total) const {
-    return ((1 - descuento) * total);
+    return ((1.0 - descuento) * total); // Cálculo del total a pagar con el descuento aplicado
 }
--- Tareas/Tarea2/src/ClientePremium.hpp
+++ Tareas/Tarea2/src/ClientePremium.hpp
@@ -1,16 +1,60 @@
+/**
+ * @file ClientePremium.hpp
+ * @brief Archivo que contiene la declaración de la clase ClientePremium.
+ * 
+ * @details En este header file se declara los atributos y miembros de la clase ClientePremium.
+ * Esta clase hereda los miembros de la clase abstracta Cliente y añade el atributo privado de descuento.
+ * Además, incluye métodos como el constructor y la implementación de la función virtual pura aplicarDescuento().
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 06/09/2024
+ */
+
 #ifndef CLIENTE_PREMIUM_HPP
 #define CLIENTE_PREMIUM_HPP
 
 #include <iostream>
+
+// Header file que contiene la declaración de la clase Cliente
 #include "Cliente.hpp"
 
+/**
+ * @class ClientePremium
+ * @brief Representa un cliente del tipo premium en la tienda.
+ * 
+ * La clase ClientePremium hereda los miembros de la clase Cliente de forma pública.
+ * Implementa un constructor y la función aplicarDescuento() sobrescrita.
+ */
 class ClientePremium : public Cliente {
     private:
+        /// @brief Descuento específico para el cliente premium.
         double descuento;
 
     public:
+        /**
+         * @brief Constructor de la clase ClientePremium.
+         * 
+         * @param nombre Nombre del cliente premium.
+         * @param descuento Descuento a aplicarle al cliente premium en las compras. Rango: [0-1[
+         */
         ClientePremium(std::string &nombre, double descuento);
 
+        /**
+         * @brief Aplica el descuento al precio total a pagar.
+         * 
+         * Método sobrescrito de la clase abstracta Cliente que aplica el descuento al total a pagar
+         * para el cliente premium.
+         * 
+         * @param total Total a pagar.
+         * 
+         * @return Precio final con descuento aplicado.
+         */
         double aplicarDescuento(double total) const override;
 };
 
