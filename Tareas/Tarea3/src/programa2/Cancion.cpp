f05a08f6ddcb7e4984b2450e48734cb491498294
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-14 11:05:30 -0600

    Agregando documentacion Cancion.cpp
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea3/src/programa2/Cancion.cpp
+++ Tareas/Tarea3/src/programa2/Cancion.cpp
@@ -1,12 +1,27 @@
+/**
+ * @file Cancion.cpp
+ * @brief Archivo que contiene la definición de las funciones miembro de la clase Cancion.
+ * 
+ * @details En este source file se implementan las funciones miembro declaradas en el header file Cancion.hpp.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 14/09/2024
+ */
 
-#include "Cancion.hpp"
+#include "Cancion.hpp" // Incluir header file con las declaraciones
 
 using namespace std;
 
-// Constructor
+// Definición del constructor de la clase Cancion: Se asignan los parámetros a cada uno de las variables miembro implementadas
 Cancion::Cancion(const string &nombre, const string &artista, double duracion) : nombre(nombre), artista(artista), duracion(duracion) {}
 
-// Getters
+/* Definición de funciones getters */
 
 string Cancion::getNombre() const {
     return nombre;
@@ -19,6 +34,8 @@ string Cancion::getArtista() const {
 double Cancion::getDuracion() const {
     return duracion;
 }
+
+/* Definición de funciones setters */
 
 void Cancion::setNombre(const std::string &nombre) {
     this->nombre = nombre; // Uso de this para diferenciar del parámetro
