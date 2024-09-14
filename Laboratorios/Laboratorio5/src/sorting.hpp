8fdd976957ae02d871ec7794cc68afc9677d11ed
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-13 19:04:08 -0600

    Agregando descripcion de parametros y retorno
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Laboratorios/Laboratorio5/src/sorting.hpp
+++ Laboratorios/Laboratorio5/src/sorting.hpp
@@ -13,15 +13,19 @@
 using namespace std;
 
 // Declaración de algoritmo de ordenamiento bubble Sort
+// Recibe el vector a ordenar
 void bubble_sort(vector<int>& arr);
 
 // Declaración de algoritmo de ordenamiento selection Sort
+// Recibe el vector a ordenar
 void selection_sort(vector<int>& arr);
 
 // Declaración de algoritmo de ordenamiento insertion Sort
+// Recibe el vector a ordenar
 void insertion_sort(vector<int>& arr);
 
 // Función para generar un vector de números aleatorios
+// Recibe el tamaño del vector a crear y retorna el vector compuesto por números aleatorios
 vector<int> generate_random_vector(size_t size);
 
 #endif // SORTING_HPP
