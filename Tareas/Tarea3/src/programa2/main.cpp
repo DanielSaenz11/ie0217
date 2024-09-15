5d0778d5187aec8341f61c8520545c1d2aa6d5f5
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-14 21:01:37 -0600

    Agregando documentacion y mejorando input usuario
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea3/src/programa2/main.cpp
+++ Tareas/Tarea3/src/programa2/main.cpp
@@ -1,8 +1,24 @@
+/**
+ * @file main.cpp
+ * @brief Archivo que contiene a la función main() y el menú del programa.
+ * 
+ * @details En este source file se implementa el menú del programa de lista de canciones y el sistema de
+ * validación de entradas del usuario.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 14/09/2024
+ */
 
-
-#include "ListaCanciones.hpp"
-#include "Cancion.hpp"
+#include "ListaCanciones.hpp" // Clase ListaCanciones
+#include "Cancion.hpp" // Clase Cancion
 #include <iostream>
+#include <limits> // Para std::numeric_limits
 
 using namespace std;
 
@@ -18,16 +34,27 @@ enum OpcionesMenu {
     OPT_SALIR
 };
 
+/**
+ * @brief Punto de entrada de ejecución del programa.
+ * 
+ * Se encarga de mostrar el menú y de validar las entradas del usuario, así como de solicitar los datos
+ * requeridos dependiendo de la opción escogida.
+ * 
+ * @return Código de estado del programa.
+ */
 int main() {
+
+    // Declaraciónd de lista de canciones
     ListaCanciones lista;
 
-    // Agregar canciones
-    lista.insertarFinal(Cancion("C1", "A1", 1.5));
-    lista.insertarFinal(Cancion("C2", "A2", 2.5));
-    lista.insertarFinal(Cancion("C3", "A3", 3));
+    // Agregar canciones al final de la lista
+    lista.insertarFinal(Cancion("Not Like Us", "Kendrick Lamar", 4.5));
+    lista.insertarFinal(Cancion("Rockstar", "Post Malone", 4));
+    lista.insertarFinal(Cancion("Runaway", "Kanye West", 4.5));
 
-    int opcion;
+    int opcion; // Opcion ingresada por el usuario
 
+    // do-while loop: Muestra el menú de forma recurrente
     do {
         // Mostrar menú
         cout << "\n*-* Programa de Gestión de Canciones *-*" << endl;
@@ -37,36 +64,48 @@ int main() {
         cout << "4. Imprimir lista de canciones" << endl;
         cout << "5. Salir" << endl;
 
-        cout << "Seleccione una opción: " << endl;
+        // Ingreso de opción
+        cout << "Seleccione una opción: ";
         cin >> opcion;
 
+        // Declaración de variables utilizadas para recibir datos del usuario
         string nombre, artista;
         double duracion;
 
+        // Validación de opciones
         switch (opcion) {
             case OPT_INSERTAR_CANCION: {
+
+                // Solicitar datos de la canción a agregar
+
                 cout << "Nombre: ";
-                cin >> nombre;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Limpiar buffer de entrada cuando antes hubo un ingreso con cin
+                getline(cin, nombre); // Leer línea
 
                 cout << "Artista: ";
-                cin >> artista;
+                getline(cin, artista); // Leer línea
 
                 cout << "Duracion (min): ";
                 cin >> duracion;
 
-                lista.insertarFinal(Cancion(nombre, artista, duracion));
+                // Llamado de función para insertar canción -> Se solicita el tipo de inserción
+                lista.insertarCancion(Cancion(nombre, artista, duracion));
 
                 break;
             }
             case OPT_ELIMINAR_CANCION: {
                 cout << "Nombre de la canción a eliminar: ";
-                cin >> nombre;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Limpiar buffer de entrada cuando antes hubo un ingreso con cin
+                getline(cin, nombre); // Leer línea
+
                 lista.eliminarCancion(nombre);
                 break;
             }
             case OPT_MODIFICAR_CANCION: {
                 cout << "Nombre de la canción a modificar: ";
-                cin >> nombre;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Limpiar buffer de entrada cuando antes hubo un ingreso con cin
+                getline(cin, nombre); // Leer línea
+
                 lista.modificarCancion(nombre);
                 break;
             }
@@ -83,7 +122,7 @@ int main() {
                 break;
         }
 
-    } while(opcion != OPT_SALIR);
+    } while(opcion != OPT_SALIR); // Condición de repetición de do-while loop
 
     return 0;
 }
