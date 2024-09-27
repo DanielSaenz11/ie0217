726a16b6c2ec988f334e423f6f8b3abc48e1ece7
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-26 21:33:07 -0600

    Agregando EventosMouse2 que muestra la posicion relativa del cursor respecto al boton
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion10/EventosMouse2/MainFrame.cpp
@@ -0,0 +1,31 @@
+/*
+Archivo que contiene la definición del constructor de la clase MainFrame, así como de las funciones declaradas
+para manejar eventos de mouse.
+*/
+
+#include "MainFrame.hpp"
+#include <wx/wx.h>
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+	wxPanel* panel = new wxPanel(this); // Asignar panel a MainFrame
+
+	// Crear botón dentro de panel, en la posición (300, 250) y tamaño 200x100
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));
+
+	// Crear barra de estado abajo de la ventana
+	wxStatusBar* statusBar = CreateStatusBar();
+	statusBar->SetDoubleBuffered(true); // Contrarrestar el parpadeo del mouse
+
+	// Asociar (bindear) evento de mouse dentro del MainFrame a la función definida para modificar el status bar
+	panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);  // Se muestra la posición continuamente (sólo con movimiento del cursor)
+	button->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this); // Se ejecuta al mover el cursor dentro del botón
+	// Se evidencia la posición relativa dentro del panel/boton pues se muestra las coordenadas RELATIVAS a estos dependiendo de dónde está el cursor
+}
+
+// Definición de función cuando hay eventos de mouse
+void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
+	wxPoint mousePos = evt.GetPosition();
+	wxString message = wxString::Format("Mouse Event Detected! (x=%d, y=%d)", mousePos.x, mousePos.y);
+	wxLogStatus(message);
+}
+
