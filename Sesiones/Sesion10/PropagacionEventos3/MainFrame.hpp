4a1e569e3ed4b40d98528dcce16313be5749bf2f
Daniel Sáenz <dsaenzobando@gmail.com>
2024-09-26 21:16:15 -0600

    Agregando ejercicio de PropagacionEventos3 que implementa dos funciones a los botones de la interfaz
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Sesiones/Sesion10/PropagacionEventos3/MainFrame.hpp
@@ -0,0 +1,18 @@
+/*
+Archivo que contiene la declaración de la clase MainFrame
+
+Abarca el constructor de la clase y las funciones que se ejecuta al presionar un botón dentro de la ventana MainFrame
+*/
+
+#include <wx/wx.h>
+
+class MainFrame : public wxFrame {
+public:
+	// Contructor de MainFrame: Recibe título de la ventana principal
+	MainFrame(const wxString& title);
+
+private:
+	void OnAnyButtonClicked(wxCommandEvent& evt); // Evento cuando de hace clic en cualquier botón
+	void OnButton1Clicked(wxCommandEvent& evt);   // Evento cuando de hace clic en el botón 1
+	void OnButton2Clicked(wxCommandEvent& evt);   // Evento cuando de hace clic en el botón 2
+};
---
+++ Sesiones/Sesion10/PropagacionEventos3/MyApp.hpp
@@ -0,0 +1,13 @@
+/*
+Header file para la clase App
+Contiene el método OnInit() que se ejecuta al iniciar la aplicación
+*/
+
+#include <wx/wx.h>
+
+// Declaración de clase App: Hereda públicamente de wxApp
+class App : public wxApp {
+public:
+	// Método por donde entra la aplicación en la ejecución
+	bool OnInit();
+};
