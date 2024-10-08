0782410f9881c383d1c88ee075e95b2ff2b1ea42
Daniel Sáenz <dsaenzobando@gmail.com>
2024-10-08 09:26:38 -0600

    Agregando comentarios a AWS
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Sesiones/Sesion12/AWS/aws.cpp
+++ Sesiones/Sesion12/AWS/aws.cpp
@@ -1,3 +1,12 @@
+/*
+Este programa es utilizado para interactuar con el servicio de nube AWS
+
+Se crea una tabla llamada test_table y se agregan los valores 'John Doe' y 'Jane Doe'
+Finalmente, muestra los valores colocados en la tabla por medio de SELECT en la terminal
+
+Se manejan las excepciones con try-catch y se imprime en dónde ocurrió la excepción, el código de error y el estado del SQL.
+*/
+
 #include <mysql_driver.h>
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
@@ -19,7 +28,7 @@ int main() {
 
         // Conectarse a la base de datos de Amazon RDS
         driver = sql::mysql::get_mysql_driver_instance();
-        con = driver->connect("tcp://db-ie0217.cxecqq0wcyiv.us-east-2.rds.amazonaws.com:3306", "<nombreUsuario>", "<masterPassword>");
+        con = driver->connect("tcp://<ENDPOINT>:3306", "<NOMBRE_USUARIO>", "<MASTER_PASSWORD>");
 
         // Seleccionar base de datos
         con->setSchema("IE0217");
@@ -34,15 +43,18 @@ int main() {
         // Seleccionar y mostrar datos
         res = stmt->executeQuery("SELECT * FROM test_table");
 
+        // Recorrer elementos de resultado e imprimirlos por pantalla
         while(res->next()) {
             cout << "id = " << res->getInt("id");
             cout << ", name = " << res->getString("name") << endl;
         }
 
+        // Eliminar las variables utilizadas para recibir la información
         delete res;
         delete stmt;
         delete con;
     } catch (sql::SQLException &e) {
+        // Imprimir información del error ocurrido
         cout << "# ERR: SQLException in " << __FILE__;
         cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
         cout << "# ERR: " << e.what();
