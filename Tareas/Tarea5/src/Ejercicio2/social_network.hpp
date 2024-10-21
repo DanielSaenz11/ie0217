e07099e29b010d3e654e364db0d07d61d222cb52
Daniel Sáenz <dsaenzobando@gmail.com>
2024-10-20 22:20:05 -0600

    Agregando declaracion de funciones para interactuar con la base de datos MongoDB
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

---
+++ Tareas/Tarea5/src/Ejercicio2/social_network.hpp
@@ -0,0 +1,29 @@
+
+#ifndef SOCIAL_NETWORK_HPP
+#define SOCIAL_NETWORK_HPP
+
+#include <iostream>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <string>
+
+void insertarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& contenido, const std::string& fecha);
+
+void consultarPublicaciones(mongocxx::collection& collection);
+
+void consultarPublicacionesPorAutor(mongocxx::collection& collection, std::string& autor);
+
+void agregarComentario(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, const std::string& usuario, const std::string& comentario, const std::string& fechaComentario);
+
+void actualizarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, const std::string& nuevoContenido);
+
+void eliminarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& fecha);
+
+void marcarDestacada(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, bool destacada);
+
+bool verificarFormatoFecha(const std::string& fecha);
+
+#endif // SOCIAL_NETWORK_HPP
