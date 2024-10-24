a515d660a46d75a9ca7bf1ce7d8f5b81d8e4c41f
Daniel Sáenz <dsaenzobando@gmail.com>
2024-10-24 08:30:07 -0600

    Agregando documentacion en Doxygen y comentarios
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea5/src/Ejercicio2/main.cpp
+++ Tareas/Tarea5/src/Ejercicio2/main.cpp
@@ -1,5 +1,27 @@
+/**
+ * @file main.cpp
+ * @brief Archivo que muestra el menú de interacción del usuario con la base de datos MongoDB.
+ * 
+ * @details En este archivo se encuentra la función main, la cual corresponde al punto de inicio de ejecución del
+ * programa. Se manejan las entradas del usuario y las acciones que desea realizar sobre la base de datos. Asimismo,
+ * contiene la información de la base de datos (nombre y colección) para conectarse a ella a través del localhost.
+ * 
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 25/10/2024
+ */
+
 #include "social_network.hpp"
 
+/**
+ * @enum OpcionesMenu
+ * @brief Contiene las opciones del menú del programa.
+ */
 enum opcionesMenu {
     OPT_INSERTAR_PUBLICACION = 1,
     OPT_CONSULTAR_TODAS_PUBLICACIONES,
@@ -12,13 +34,21 @@ enum opcionesMenu {
     OPT_SALIR
 };
 
+/**
+ * @brief Función principal que controla la interacción con el usuario.
+ * 
+ * Proporciona un menú interactivo para realizar operaciones CRUD sobre la base de datos social_network en MongoDB.
+ * 
+ * @return int Código de salida del programa.
+ */
 int main() {
-    mongocxx::instance inst{};
-    mongocxx::client client{mongocxx::uri{}};
-    mongocxx::database db = client["social_network"];
-    mongocxx::collection collection = db["posts"];
+    // Inicializar la instancia de MongoDB y establecer la conexión
+    mongocxx::instance inst{};                          // Instancia
+    mongocxx::client client{mongocxx::uri{}};           // Cliente
+    mongocxx::database db = client["social_network"];   // Base de datos
+    mongocxx::collection collection = db["posts"];      // Colección
 
-    int opcion = 0;
+    int opcion = 0; // Opción ingresada por el usuario en el menú
     do {
         // Mostrar el menú de opciones
         std::cout << "\n--- Menú de Opciones ---" << std::endl;
@@ -33,14 +63,17 @@ int main() {
         std::cout << "9. Salir" << std::endl;
         std::cout << "Seleccione una opción: ";
 
+        // Validar la entrada del usuario para el menú
         while (!(std::cin >> opcion)) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Entrada inválida. Ingrese un valor numérico entre las opciones.";
         }
 
+        // Ejecutar la acción correspondiente a la opción seleccionada
         switch (opcion) {
             case OPT_INSERTAR_PUBLICACION: {
+                // Solicitar datos para insertar una nueva publicación
                 std::string autor, contenido, fecha;
 
                 std::cout << "Ingrese el autor: ";
@@ -53,7 +86,9 @@ int main() {
                 std::cout << "Ingrese la fecha (YYYY-MM-DD): ";
                 std::getline(std::cin, fecha);
 
+                // Validar el formato de la fecha
                 if (verificarFormatoFecha(fecha)) {
+                    // Insertar publicación a la base de datos con la información dada
                     insertarPublicacion(collection, autor, contenido, fecha);
                 } else {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
@@ -61,9 +96,11 @@ int main() {
                 break;
             }
             case OPT_CONSULTAR_TODAS_PUBLICACIONES:
+                // Consultar y mostrar todas las publicaciones
                 consultarPublicaciones(collection);
                 break;
             case OPT_CONSULTAR_POR_AUTOR: {
+                // Solicitar el autor y consultar sus publicaciones
                 std::string autor;
 
                 std::cout << "Ingrese el autor: ";
@@ -74,9 +111,11 @@ int main() {
                 break;
             }
             case OPT_CONSULTAR_POR_FECHA:
+                // Consultar y mostrar las publicaciones por fecha
                 consultarPublicacionesPorFecha(collection);
                 break;
             case OPT_AGREGAR_COMENTARIO: {
+                // Solicitar datos para agregar un comentario a una publicación existente
                 std::string autor, fecha, usuario, comentario, fechaComentario;
 
                 std::cout << "Ingrese el autor de la publicación: ";
@@ -86,6 +125,7 @@ int main() {
                 std::cout << "Ingrese la fecha de la publicación (YYYY-MM-DD): ";
                 std::getline(std::cin, fecha);
 
+                // Validar el formato de la fecha
                 if (!verificarFormatoFecha(fecha)) {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
                     break;
@@ -100,7 +140,9 @@ int main() {
                 std::cout << "Ingrese la fecha del comentario (YYYY-MM-DD): ";
                 std::getline(std::cin, fechaComentario);
 
+                // Validar el formato de la fecha del comentario
                 if (verificarFormatoFecha(fechaComentario)) {
+                    // Agregar comentario a la base de datos con la información dada
                     agregarComentario(collection, autor, fecha, usuario, comentario, fechaComentario);
                 } else {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
@@ -108,6 +150,7 @@ int main() {
                 break;
             }
             case OPT_ACTUALIZAR_CONTENIDO: {
+                // Solicitar datos para actualizar el contenido de una publicación
                 std::string autor, fecha, nuevoContenido;
 
                 std::cout << "Ingrese el autor de la publicación: ";
@@ -117,18 +160,22 @@ int main() {
                 std::cout << "Ingrese la fecha de la publicación (YYYY-MM-DD): ";
                 std::getline(std::cin, fecha);
 
+                // Validar el formato de la fecha
                 if (!verificarFormatoFecha(fecha)) {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
                     break;
                 }
 
+                // Ingresar el nuevo contenido de la publicación
                 std::cout << "Ingrese el nuevo contenido: ";
                 std::getline(std::cin, nuevoContenido);
 
+                // Ingresar el nuevo contenido a la base de datos
                 actualizarPublicacion(collection, autor, fecha, nuevoContenido);
                 break;
             }
             case OPT_ELIMINAR_PUBLICACION: {
+                // Solicitar datos para eliminar una publicación
                 std::string autor, fecha;
 
                 std::cout << "Ingrese el autor de la publicación: ";
@@ -138,7 +185,9 @@ int main() {
                 std::cout << "Ingrese la fecha de la publicación (YYYY-MM-DD): ";
                 std::getline(std::cin, fecha);
 
+                // Validar el formato de la fecha
                 if (verificarFormatoFecha(fecha)) {
+                    // Eliminar publicación de la base de datos
                     eliminarPublicacion(collection, autor, fecha);
                 } else {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
@@ -146,6 +195,7 @@ int main() {
                 break;
             }
             case OPT_MARCAR_DESTACADA: {
+                // Solicitar datos para marcar/desmarcar una publicación como destacada
                 std::string autor, fecha;
                 bool destacada;
 
@@ -156,24 +206,29 @@ int main() {
                 std::cout << "Ingrese la fecha de la publicación (YYYY-MM-DD): ";
                 std::getline(std::cin, fecha);
 
+                // Validar el formato de la fecha
                 if (!verificarFormatoFecha(fecha)) {
                     std::cout << "Formato de fecha inválido. Use el formato YYYY-MM-DD." << std::endl;
                     break;
                 }
 
+                // Mensaje para marcar/desmarcar
                 std::cout << "¿Desea marcar la publicación como destacada? (1 para sí, 0 para no): ";
                 std::cin >> destacada;
 
+                // Actualizar el estado de destacada en la base de datos
                 marcarDestacada(collection, autor, fecha, destacada);
                 break;
             }
             case OPT_SALIR:
+                // Salir del programa
                 std::cout << "Saliendo del programa..." << std::endl;
                 break;
             default:
+                // Mensaje de opción inválida
                 std::cout << "Opción inválida. Seleccione una opción válida." << std::endl;
         }
-    } while (opcion != OPT_SALIR);
+    } while (opcion != OPT_SALIR); // Condición de repetición del loop del menú
 
     return 0;
 }
--- Tareas/Tarea5/src/Ejercicio2/social_network.cpp
+++ Tareas/Tarea5/src/Ejercicio2/social_network.cpp
@@ -1,3 +1,20 @@
+/**
+ * @file social_network.cpp
+ * @brief Archivo que contiene la implementación de las funciones para interactuar con la base de datos MongoDB.
+ * 
+ * @details Este archivo contiene la definición de las funciones para insertar y consultar sobre las publicaciones,
+ * agregar comentarios, actualizar el contenido de una publicación, eliminar una publicación y marcar como destacada.
+ *  
+ * Software bajo la licencia de MIT.
+ * En términos generales, se concede permiso para utilizar este Software sin ninguna restricción, pero se proporciona sin
+ * ninguna garantía. 
+ * El aviso de copyright anterior y este aviso de permiso deberán incluirse en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @date 25/10/2024
+ */
+
 #include "social_network.hpp"
 #include <regex>
 
@@ -6,21 +23,25 @@ using bsoncxx::builder::stream::document;
 
 void insertarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& contenido, const std::string& fecha) {
     try {
-
+        // Verificar que no haya un documento con el autor y fecha que se van a ingresar
         if (verificarPublicacionExiste(collection, autor, fecha)) {
             std::cout << "Ya existe una publicación para ese autor y fecha." << std::endl;
-            return;
+            return; // En caso de haberlo, se sale de la función
         }
 
+        // Crear documento BSON para la nueva publicación
         document publicacion{};
         publicacion << "autor" << autor
                     << "contenido" << contenido
                     << "fecha" << fecha
                     << "comentarios" << open_array << close_array
                     << "destacada" << false;
+        
+        // Insertar documento en la colección
         collection.insert_one(publicacion.view());
         std::cout << "Publicación insertada correctamente en la base de datos." << std::endl;
     } catch(const std::exception& e) {
+        // Manejo de errores en caso de fallo al insertar
         std::cerr << "Error al insertar la publicación: " << e.what() << std::endl;
     }
 }
@@ -73,7 +94,7 @@ void consultarPublicacionesPorFecha(mongocxx::collection& collection) {
 
         // Obtener los documentos ordenados por fecha
         auto cursor = collection.find({}, mongocxx::options::find{}.sort(sortOptions.view()));
-        
+
         // Imprimir cada documento con formato específico
         for (auto&& doc : cursor) {
             std::cout << "Autor: " << doc["autor"].get_utf8().value << "\n"
@@ -89,112 +110,131 @@ void consultarPublicacionesPorFecha(mongocxx::collection& collection) {
 
 void agregarComentario(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, const std::string& usuario, const std::string& comentario, const std::string& fechaComentario) {
     try {
-
+        // Verificar si existe una publicación para comentar con el autor y fecha indicados
         if (!verificarPublicacionExiste(collection, autor, fecha)) {
             std::cout << "La publicación no existe. No se puede agregar el comentario." << std::endl;
-            return;
+            return; // En caso de no haberlo, se sale de la función
         }
 
+        // Crear filtro para encontrar la publicación
         document filtro{};
         filtro << "autor" << autor
                << "fecha" << fecha;
-            
+        
+        // Crear documento BSON para el nuevo comentario
         document docComentario{};
         docComentario << "usuario" << usuario
                       << "comentario" << comentario
                       << "fecha_comentario" << fechaComentario;
         
+        // Actualizar la información añadida del nuevo comentario en la publicación original
         document update{};
         update << "$push" << open_document << "comentarios" << docComentario << close_document;
 
+        // Actualizar en la colección
         collection.update_one(filtro.view(), update.view());
         
         std::cout << "Comentario agregado correctamente." << std::endl;
 
     } catch(const std::exception& e) {
+        // Manejo de errores en caso de fallo del comentario
         std::cerr << "Error al agregar comentario: " << e.what() << '\n';
     }
 }
 
 void actualizarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, const std::string& nuevoContenido) {
     try {
-
+        // Verificar si existe una publicación para actualizar su información según el autor y fecha
         if (!verificarPublicacionExiste(collection, autor, fecha)) {
             std::cout << "La publicación no existe. No se puede actualizar el contenido." << std::endl;
-            return;
+            return; // En caso de que no, se sale de la función
         }
 
+        // Crear documento para encontrar la publicación
         document filtro{};
         filtro << "autor" << autor
                << "fecha" << fecha;
         
+        // Crear documento para la actualización del nuevo contenido
         document update{};
         update << "$set" << open_document << "contenido" << nuevoContenido << close_document;
 
+        // Realizar la actualización en la colección
         collection.update_one(filtro.view(), update.view());
 
         std::cout << "Publicación actualizada correctamente." << std::endl;
 
     } catch(const std::exception& e) {
+        // Manejo de errores en caso de fallo al actualizar
         std::cerr << "Error al actualizar la publicación: " << e.what() << '\n';
     }
 }
 
 void eliminarPublicacion(mongocxx::collection& collection, const std::string& autor, const std::string& fecha) {
     try {
-
+        // Verificar si existe una publicación para eliminar
         if (!verificarPublicacionExiste(collection, autor, fecha)) {
             std::cout << "La publicación no existe para ese autor y fecha." << std::endl;
-            return;
+            return; // En caso de que no, se sale de la función
         }
 
+        // Crear filtro para encontrar la publicación
         document filtro{};
         filtro << "autor" << autor
                << "fecha" << fecha;
 
+        // Eliminar el documento que hizo match con el filtro
         collection.delete_one(filtro.view());
 
         std::cout << "Publicación eliminada correctamente." << std::endl;
 
     } catch(const std::exception& e) {
+        // Manejo de errores en caso de fallo al eliminar
         std::cerr << "Error al eliminar la publicación: " << e.what() << std::endl;
     }
 }
 
 void marcarDestacada(mongocxx::collection& collection, const std::string& autor, const std::string& fecha, bool destacada) {
     try {
-
+        // Verificar si existe una publicación para marcar como destacada según el autor y fecha
         if (!verificarPublicacionExiste(collection, autor, fecha)) {
             std::cout << "La publicación no existe. No se puede marcar/desmarcar como destacada." << std::endl;
-            return;
+            return; // En caso de que no, se sale de la función
         }
 
+        // Crear filtro para encontrar la publicación
         document filtro{};
         filtro << "autor" << autor
                << "fecha" << fecha;
 
+        // Crear documento para la marcar/desmarcar como destacada
         document update{};
         update << "$set" << open_document << "destacada" << destacada << close_document;
 
+        // Realizar la actualización en la colección
         collection.update_one(filtro.view(), update.view());
 
         std::cout << "Publicación marcada/desmarcada como destacada correctamente." << std::endl;
 
     } catch(const std::exception& e) {
+        // Manejo de errores en caso de fallo al actualizar
         std::cerr << "Error al marcar/desmarcar la publicación como destacada: " << e.what() << std::endl;
     }
 }
 
 bool verificarFormatoFecha(const std::string& fecha) {
-    std::regex formatoFecha("\\d{4}-\\d{2}-\\d{2}");
-    return std::regex_match(fecha, formatoFecha);
+    std::regex formatoFecha("\\d{4}-\\d{2}-\\d{2}"); // Formato de regex para validar la fecha (YYYY-MM-DD)
+    return std::regex_match(fecha, formatoFecha); // Retornar si hubo match
 }
 
 bool verificarPublicacionExiste(mongocxx::collection& collection, const std::string& autor, const std::string& fecha) {
+    // Crear documento de filtro
     document filter{};
-    filter << "autor" << autor << "fecha" << fecha;
+    filter << "autor" << autor << "fecha" << fecha; // Filtrar por autor y fecha
 
+    // Buscar en la colección con base en el filtro
     auto result = collection.find_one(filter.view());
 
+    // Retornar si hubo resultado o no
     return result ? true : false; // Uso de operador ternario para validar la salida
 }
