8cb5b88c6eb027d6d733bb6b41b43273d2af5ac6
DanielSaenz11 <dsaenzobando@gmail.com>
2024-10-29 18:06:18 -0600

    Agregando ej1 Lab9
    Signed-off-by: DanielSaenz11 <dsaenzobando@gmail.com>

--- Laboratorios/Laboratorio9/src/ej1.cpp
+++ Laboratorios/Laboratorio9/src/ej1.cpp
@@ -2,32 +2,39 @@
 #include <vector>
 #include <algorithm>
 
+// Función que calcula la media aritmética de un vector de elementos
 double calcularMedia(const std::vector<int>& numeros) {
-    int suma = 0;
-    for (int i = 0; i <= numeros.size(); i++) {
-        suma += numeros[i];
+    double suma = 0.0; // Se colocó como tipo de dato double a la suma
+
+    // Se ajustaron los límites del for para que únicamente llegue al último elemento. Anteriormente se obtenía un comportamiento indefinido
+    for (int i = 0; i < numeros.size(); i++) {
+        suma += numeros[i]; // Suma de cada uno de los elementos del vector
     }
-    return suma / numeros.size();
+    return suma / numeros.size(); // Retornar media (suma_total / total_elementos)
 }
 
+// Función que calcula la mediana de un vector
 double calcularMediana(std::vector<int> numeros) {
-    std::sort(numeros.begin(), numeros.end());
+    std::sort(numeros.begin(), numeros.end()); // Ordena el vector
 
-    int tam = numeros.size();
+    int tam = numeros.size(); // Variable que contiene el tamaño del vector
     if (tam % 2 == 0) {
-        return (numeros[tam/2 - 1] + numeros[tam/2]) / 2;
+        // En caso de ser par, se retorna el promedio entre sus dos elementos en el medio
+        return (numeros[tam/2 - 1] + numeros[tam/2]) / 2.0; // Se colocó 2.0 para que al hacer la división, se retorne un valor con decimales
     } else {
+        // Si el tamaño es impar, se retorna el elemento medio
         return numeros[tam/2];
     }
 }
 
 int main() {
-    std::vector<int> numeros = {20, 20, 20, 40, 50};
+    // Declaración de vector
+    std::vector<int> numeros = {1, 4, 8, 110, 33, 49};
 
     double media = calcularMedia(numeros);
     double mediana = calcularMediana(numeros);
 
-    std::cout << "Media: " << media << std::endl;
+    // std::cout << "Media: " << media << std::endl;
     std::cout << "Mediana: " << mediana << std::endl;
 
     return 0;
