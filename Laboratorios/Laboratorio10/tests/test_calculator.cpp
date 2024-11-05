c6b39830ece2f060472b325b28b7df2724c28188
Daniel Sáenz <dsaenzobando@gmail.com>
2024-11-05 17:46:04 -0600

    Agregando comentarios a los tests
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Laboratorios/Laboratorio10/tests/test_calculator.cpp
+++ Laboratorios/Laboratorio10/tests/test_calculator.cpp
@@ -1,26 +1,39 @@
-// test_calculator.cpp
+/*
+Archivo que contiene las pruebas para las diferentes operaciones dentro de la Calculadora
+
+- Suma
+- Resta
+- Multiplicación
+- División
+- División por cero
+*/
 
 #include <gtest/gtest.h>
 #include "../src/Calculator.hpp"
 
-Calculator calc;
+Calculator calc; // Crear instancia de la calculadora
 
+// Test para verificar el funcionamiento de la suma
 TEST(CalculatorTests, Addition) {
     EXPECT_DOUBLE_EQ(calc.add(3.0, 4.0), 7.0);
 }
 
+// Test para verificar el funcionamiento de la resta
 TEST(CalculatorTests, Subtraction) {
     EXPECT_DOUBLE_EQ(calc.subtract(10.0, 4.0), 6.0);
 }
 
+// Test para verificar el funcionamiento de la multiplicación
 TEST(CalculatorTests, Multiplication) {
     EXPECT_DOUBLE_EQ(calc.multiply(3.0, 5.0), 15.0);
 }
 
+// Test para verificar el funcionamiento de la división
 TEST(CalculatorTests, Division) {
     EXPECT_DOUBLE_EQ(calc.divide(10.0, 2.0), 5.0);
 }
 
+// Test para verificar el funcionamiento de la división por cero
 TEST(CalculatorTests, DivisionByZero) {
     EXPECT_THROW(calc.divide(10.0, 0), std::invalid_argument);
 }
