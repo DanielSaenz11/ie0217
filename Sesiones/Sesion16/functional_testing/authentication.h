a100efbdfd017b1a69a8ba3c12a5cda6cb783644
Daniel Sáenz <dsaenzobando@gmail.com>
2024-11-04 21:15:20 -0600

    Agregando clase Authentication que permite hacer login y registerUser
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Sesiones/Sesion16/functional_testing/authentication.h
+++ Sesiones/Sesion16/functional_testing/authentication.h
@@ -1,3 +1,6 @@
+/*
+Archivo que contiene la declaración y definición de las funciones login y registerUser en la clase Authentication
+*/
 
 #ifndef AUTHENTICATION_H
 #define AUTHENTICATION_H
@@ -5,25 +8,33 @@
 #include <string>
 #include "database.h"
 
+// Clase Authentication
 class Authentication {
     public:
+        // Constructor de la clase: Recibe una instancia de la base de datos
         Authentication(Database* db) : db_(db) {}
 
-            bool login(const std::string& username, const std::string& password) {
-                if (db_->userExists(username)) {
-                    return db_->getPassword(username) == password;
-                }
-                return false;
+        // Método para iniciar sesión (con el username y password)
+        bool login(const std::string& username, const std::string& password) {
+            // Si existe el nombre de usuario dentro de la base de datos
+            if (db_->userExists(username)) {
+                // Retorna un booleano para indicar si el password ingresado coincide con el de la cuenta
+                return db_->getPassword(username) == password;
             }
-
-            void registerUser(const std::string& username, const std::string& password) {
-                if (!db_->userExists(username)) {
-                    db_->addUser(username, password);
-                }
+            return false;
+        }
+        
+        // Método para registrar un usuario en la base de datos (requiere el username y el password)
+        void registerUser(const std::string& username, const std::string& password) {
+            // Si no existe el usuario en la base de datos
+            if (!db_->userExists(username)) {
+                // Agrega el usuario a la base de datos con su nombre de usuario y password
+                db_->addUser(username, password);
             }
+        }
 
     private:
-        Database* db_;
+        Database* db_; // Instancia privada de la base de datos
 };
 
 #endif // AUTHENTICATION_H
