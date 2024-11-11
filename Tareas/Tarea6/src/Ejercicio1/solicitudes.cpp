75c3b662e1c0f0e2709c8d10458e3de1684f54bb
Daniel Sáenz <dsaenzobando@gmail.com>
2024-11-10 19:50:34 -0600

    Agregando implementacion de funciones de solicitudes para el sistema
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- Tareas/Tarea6/src/Ejercicio1/solicitudes.cpp
+++ Tareas/Tarea6/src/Ejercicio1/solicitudes.cpp
@@ -1,5 +1,19 @@
+/**
+ * @file soporte_tecnico.cpp
+ * @brief Implementación de las funciones para el sistema de soporte técnico paralelo.
+ * @details Este archivo contiene la implementación de las funciones definidas en el archivo de cabecera.
+ *          Aquí se desarrollan las operaciones de los hilos de clientes y operadores, que utilizan mecanismos
+ *          de sincronización para proteger el acceso al buffer compartido. Las funciones implementan los 
+ *          semáforos y mutex necesarios para evitar condiciones de carrera y asegurar un funcionamiento 
+ *          eficiente del sistema. También incluye la función de monitoreo de rendimiento para registrar 
+ *          el tiempo promedio de espera de los clientes y de procesamiento de los operadores.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @copyright MIT License
+ * @date 10/11/2024
+ */
 
-
+#include "solicitudes.hpp"
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -11,13 +25,10 @@
 #include <atomic>
 #include <random>
 
-using namespace std;
-
 // Configuración inicial
 constexpr int TAMANO_BUFFER = 10; // Tamaño del buffer
 std::queue<int> buffer; // Buffer modelado como una cola
 std::mutex mtx; // Mutex para proteger el acceso al buffer
-std::condition_variable cv_cliente, cv_operador; // Variables de condición para cliente y operador
 std::counting_semaphore<TAMANO_BUFFER> empty_slots(TAMANO_BUFFER); // Semáforo empty_slots: Tamaño inicial del buffer
 std::counting_semaphore<TAMANO_BUFFER> full_slots(0); // Semáforo full_slots: Tamaño inicial de 0
 
@@ -25,7 +36,10 @@ std::counting_semaphore<TAMANO_BUFFER> full_slots(0); // Semáforo full_slots: T
 std::atomic<int> total_solicitudes_procesadas{0}; // Total de solicitudes procesadas
 std::atomic<int> tiempo_espera_total_clientes{0}; // Tiempo de espera total de clientes
 std::atomic<int> tiempo_espera_total_operadores{0}; // Tiempo de espera total de operadores
-std::atomic<bool> produccion_finalizada{false}; // Booleano para indicar si la producción finalizó
+
+// Variables compartidas
+std::condition_variable cv_cliente, cv_operador;
+std::atomic<bool> produccion_finalizada{false};
 
 // Generador de tiempos aleatorios para simular tiempos de espera
 std::random_device rd;
@@ -33,7 +47,7 @@ std::mt19937 gen(rd());
 std::uniform_int_distribution<> dist(50, 150);
 
 
-// Función para los hilos de los clientes (productores)
+// Definición de función para los hilos de los clientes (productores)
 void cliente(int id, int num_solicitudes) {
     // Generar solicitudes por cliente
     for (int i = 0; i < num_solicitudes; ++i) {
@@ -55,7 +69,7 @@ void cliente(int id, int num_solicitudes) {
         {
             std::lock_guard<std::mutex> lock(mtx); // Bloquear porción de memoria compartida
             buffer.push(solicitud); // Realizar la solicitud
-            cout << "Cliente " << id << " generó la solicitud " << solicitud << endl;
+            std::cout << "Cliente " << id << " generó la solicitud " << solicitud << std::endl;
             // Desbloquear lock al salir del scope
         }
 
@@ -67,15 +81,9 @@ void cliente(int id, int num_solicitudes) {
 
         std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen))); // Espera aleatoria entre solicitudes
     }
-
-    // Indicar que todos los clientes terminaron de generar solicitudes
-    produccion_finalizada = true;
-    
-    // Notifica a todos los hilos de operadores en espera
-    cv_operador.notify_all();
 }
 
-// Función para los hilos de los operadores (consumidores)
+// Definición de función para los hilos de los operadores (consumidores)
 void operador(int id) {
     // Bucle infinito que consume tareas
     while (true) {
@@ -89,18 +97,26 @@ void operador(int id) {
         // Permite que otros hilos accedan al buffer durante la espera
         cv_operador.wait(lock, [] { return !buffer.empty() || produccion_finalizada; });
 
-        if (buffer.empty() && produccion_finalizada) break; // Terminar si no hay más solicitudes
+        if (buffer.empty() && produccion_finalizada) {
+            lock.unlock();
+            empty_slots.release();  // Liberar el espacio ocupado en caso de estar dentro del buffer
+            break; // Terminar si no hay más solicitudes
+        }
 
-        int solicitud = buffer.front(); // Obtener el elemento del frente de la cola
-        buffer.pop(); // Elimina el elemento sacado previamente
-        total_solicitudes_procesadas++; // Aumenta el número de solicitudes procesadas
-        cout << "Operador " << id << " procesó la solicitud " << solicitud << endl;
+        // Si el buffer no está vacío
+        if (!buffer.empty()) {
+            int solicitud = buffer.front(); // Obtener el elemento del frente de la cola
+            buffer.pop(); // Elimina el elemento sacado previamente
+            total_solicitudes_procesadas++; // Aumenta el número de solicitudes procesadas
+            std::cout << "Operador " << id << " procesó la solicitud " << solicitud << std::endl;
 
-        lock.unlock(); // Desbloquea el lock
-        empty_slots.release(); // Incrementa el contador de espacios libres en el buffer
+            empty_slots.release(); // Incrementa el contador de espacios libres en el buffer
+        }
         
         // Notificar a un cliente en espera de un espacio en el buffer
         cv_cliente.notify_one();
+
+        lock.unlock(); // Desbloquea el lock
 
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Tiempo de procesamiento simulado
     }
@@ -115,42 +131,9 @@ void monitorear_rendimiento(int num_clientes, int num_operadores) {
     int espera_promedio_operadores = tiempo_espera_total_operadores / (num_operadores * TAMANO_BUFFER);
     
     // Imprimir resultados
-    cout << "\n--- Métricas de Rendimiento ---" << endl;
-    cout << "Total de solicitudes procesadas: " << total_solicitudes_procesadas << endl;
-    cout << "Tiempo de espera promedio para clientes: " << espera_promedio_clientes << " ms" << endl;
-    cout << "Tiempo de espera promedio para operadores: " << espera_promedio_operadores << " ms" << endl;
-    cout << "Tamaño del buffer: " << TAMANO_BUFFER << endl;
-}
-
-int main() {
-    int num_clientes = 4;
-    int num_operadores = 3;
-    int num_solicitudes_por_cliente = 10;
-
-    vector<thread> clientes, operadores;
-
-    // Crear hilos de clientes
-    for (int i = 0; i < num_clientes; ++i) {
-        clientes.emplace_back(cliente, i, num_solicitudes_por_cliente);
-    }
-
-    // Crear hilos de operadores
-    for (int i = 0; i < num_operadores; ++i) {
-        operadores.emplace_back(operador, i);
-    }
-
-    // Esperar a que terminen todos los clientes
-    for (auto& hilo_cliente : clientes) {
-        hilo_cliente.join();
-    }
-
-    // Esperar a que terminen todos los operadores
-    for (auto& hilo_operador : operadores) {
-        hilo_operador.join();
-    }
-
-    monitorear_rendimiento(num_clientes, num_operadores);
-    cout << "Todos los clientes y operadores han terminado.\n";
-
-    return 0;
+    std::cout << "\n--- Métricas de Rendimiento ---" << std::endl;
+    std::cout << "Total de solicitudes procesadas: " << total_solicitudes_procesadas << std::endl;
+    std::cout << "Tiempo de espera promedio para clientes: " << espera_promedio_clientes << " ms" << std::endl;
+    std::cout << "Tiempo de espera promedio para operadores: " << espera_promedio_operadores << " ms" << std::endl;
+    std::cout << "Tamaño del buffer: " << TAMANO_BUFFER << std::endl;
 }
